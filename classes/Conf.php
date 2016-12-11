6ee5e95b002b5037daf5f1e5ccfbe2f9bff7a3ff
HIR0Y0SHI <bardiche.assault7@gmail.com>
2016-12-11 13:19:32 +0900

    Create Structure

---
+++ classes/Conf.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* WP32 PHPサンプル8 マスタテーブル管理DAO版 Src04/12
+* 定数記述ファイル
+*
+* @author Shinzo SAITO <architshin@websarva.com>
+* @version 1.0
+* @copyright Sarva
+*
+* ファイル名＝Conf.php
+* ディレクトリ＝/wp32/scottadmindao/classes/
+*/
+
+const DB_DNS = "mysql:host=localhost;dbname=wp32scott;charset=utf8";
+const DB_USERNAME = "scott";
+const DB_PASSWORD = "tiger";
+
+?>
